428884c2404174ce3c978c82160ac94adf770901
zombine <xxlcrackerxxl@gmail.com>
2020-01-30 15:40:05 +0300

    add sugn up form and add validation

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,8 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import i18n from "./i18n";
+import axios from "axios";
 
 Vue.config.productionTip = false;
+axios.defaults.baseURL = 'http://localhost:3000';
 
 new Vue({
     router,
